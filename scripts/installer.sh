3d01c3375d40a462a181b3b05551af19efed584e
camthegeek <cam@camthegeek.net>
2019-10-14 11:54:09 -0400

    restructuring

---
+++ scripts/installer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ ! -d src ]
+then
+mkdir src
+fi
+
+cd masari-webwallet
+npm install
+node ./node_modules/typescript/bin/tsc --project tsconfig.prod.json
+node build.js
+cd ../
+
+cp -r masari-webwallet/src/. src
+#cp -r src_specific/. src
+electron-forge start
